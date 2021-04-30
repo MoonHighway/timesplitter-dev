30478ac2522a341d4032f93e5357987092c2f4f0
MoonTahoe <alex@moonhighway.com>
2021-04-29 20:23:21 -0700

    Adds some counting functions

--- src/lib/countTopics.js
+++ src/lib/countTopics.js
@@ -1,62 +1,81 @@
-export function countByType(topic) {
-  return {
-    slides: 0,
+import { totalTime } from ".";
+
+export const countByType = (
+  topic,
+  count = {
     samples: 0,
+    slides: 0,
+    labSteps: 0,
+    exerciseSteps: 0,
+  }
+) => {
+  if (topic.type === "sample") count.samples = count.samples + 1;
+  if (topic.type === "slides") count.slides = count.slides + 1;
+  if (topic.type === "lab")
+    count.labSteps = count.labSteps + topic.children.length;
+  if (topic.type === "exercise")
+    count.exerciseSteps = count.exerciseSteps + topic.children.length;
+  if (topic.children) {
+    return topic.children.reduce((total, t) => countByType(t, total), count);
+  }
+  return count;
+};
+
+export const countByTime = (
+  topic,
+  count = {
+    samples: 0,
+    slides: 0,
     labs: 0,
     exercises: 0,
-  };
-}
+  }
+) => {
+  if (topic.type === "slides" && topic.length)
+    count.slides = count.slides + topic.length;
+  if (topic.type === "sample") count.samples = count.samples + totalTime(topic);
+  if (topic.type === "lab") count.labs = count.labs + totalTime(topic);
+  if (topic.type === "exercise")
+    count.exercises = count.exercises + totalTime(topic);
+  if (topic.children) {
+    return topic.children.reduce((total, t) => countByTime(t, total), count);
+  }
+  return count;
+};
 
-export function countByTime(topic) {
-  return {
-    slides: 0,
-    samples: 0,
-    labs: 0,
-    exercises: 0,
-  };
-}
-
-export function countByDifficulty(topic) {
-  return {
+export const countByDifficulty = (
+  topic,
+  count = {
     beginner: 0,
     intermediate: 0,
     advanced: 0,
     expert: 0,
-  };
-}
+  }
+) => {
+  if (topic.difficulty) count[topic.difficulty]++;
+  if (topic.children) {
+    return topic.children.reduce(
+      (total, t) => countByDifficulty(t, total),
+      count
+    );
+  }
+  return count;
+};
 
-export function countByRequired(topic) {
-  return {
-    slides: 0,
-    samples: 0,
-    labs: 0,
-    exercises: 0,
-  };
-}
-
-export function countByPercent(topic) {
-  return {
-    slides: 0,
-    samples: 0,
-    labs: 0,
-    exercises: 0,
-  };
-}
-
-export function countRequiredByDifficulty(topic) {
-  return {
-    beginner: 0,
-    intermediate: 0,
-    advanced: 0,
-    expert: 0,
-  };
-}
-
-export function countPercentByDifficulty(topic) {
-  return {
-    beginner: 0,
-    intermediate: 0,
-    advanced: 0,
-    expert: 0,
-  };
-}
+// export const countByRequired = (
+//   topic,
+//   count = {
+//     slides: 0,
+//     samples: 0,
+//     labs: 0,
+//     exercises: 0,
+//   }
+// ) => {
+//   if (topic.required) count[topic.type]++;
+//   if (topic.children) {
+//     return topic.children.reduce(
+//       (total, t) => countByRequired(t, total),
+//       count
+//     );
+//   }
+//   return count;
+// };
--- src/lib/countTopics.test.js
+++ src/lib/countTopics.test.js
@@ -2,79 +2,46 @@ import {
   countByType,
   countByTime,
   countByDifficulty,
-  countByRequired,
-  countByPercent,
-  countRequiredByDifficulty,
-  countPercentByDifficulty,
+  //countByRequired,
 } from ".";
 const testData = require("./countTopics.test.json");
 
 it("countByType(topic)", () => {
   let result = countByType(testData);
   expect(result).toEqual({
-    slides: 0,
-    samples: 0,
-    labs: 0,
-    exercises: 0,
+    slides: 2,
+    samples: 3,
+    labSteps: 4,
+    exerciseSteps: 5,
   });
 });
 
 it("countByTime(topic)", () => {
   let result = countByTime(testData);
   expect(result).toEqual({
-    slides: 0,
-    samples: 0,
-    labs: 0,
-    exercises: 0,
-  });
-});
-
-it("countByRequired(topic)", () => {
-  let result = countByRequired(testData);
-  expect(result).toEqual({
-    slides: 0,
-    samples: 0,
-    labs: 0,
-    exercises: 0,
-  });
-});
-
-it("countByPercent(topic)", () => {
-  let result = countByPercent(testData);
-  expect(result).toEqual({
-    slides: 0,
-    samples: 0,
-    labs: 0,
-    exercises: 0,
+    slides: 10,
+    samples: 40,
+    labs: 12,
+    exercises: 30,
   });
 });
 
 it("countByDifficulty(topic)", () => {
   let result = countByDifficulty(testData);
   expect(result).toEqual({
-    beginner: 0,
-    intermediate: 0,
-    advanced: 0,
-    expert: 0,
+    beginner: 6,
+    intermediate: 3,
+    advanced: 5,
+    expert: 5,
   });
 });
 
-it("countRequiredByDifficulty(topic)", () => {
-  let result = countRequiredByDifficulty(testData);
-  expect(result).toEqual({
-    beginner: 0,
-    intermediate: 0,
-    advanced: 0,
-    expert: 0,
-  });
-});
-
-it("countPercentByDifficulty(topic)", () => {
-  let result = countPercentByDifficulty(testData);
-  expect(result).toEqual({
-    beginner: 0,
-    intermediate: 0,
-    advanced: 0,
-    expert: 0,
-  });
-});
+// it("countByRequired(topic)", () => {
+//   let result = countByRequired(testData);
+//   expect(result).toEqual({
+//     slides: 0,
+//     samples: 0,
+//     labs: 0,
+//     exercises: 0,
+//   });
+// });
